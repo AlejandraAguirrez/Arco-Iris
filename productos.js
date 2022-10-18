3d9d4e14f55ecdaec94139512c939cdad081899f
Alejandra Aguirre <gabizar18@gmail.com>
2022-10-18 14:23:13 -0300

    imagenes inicio

--- productos.js
+++ productos.js
@@ -5,18 +5,18 @@ const contenedorCarrito = document.getElementById('carrito-contenedor')
 const listaCarrito = document.getElementById('carritoContenedor')
 
 const botonVaciar = document.getElementById('vaciar-carrito')
-const contadorCarrito = document.getElementById('contadorCarrito')
+const contador = document.getElementById('contador')
 
 const cantidad = document.getElementById('cantidad')
 const precioTotal = document.getElementById('precioTotal')
-const cantidadTotal = document.getElementById('cantidadTotal')
+// const cantidadTotal = document.getElementById('cantidadTotal')
+
 
 class Juegos{
     constructor(id, nombre, precio, img) {
         this.id = id
         this.nombre = nombre
         this.precio = precio
-        this.cantidad = 1
         this.img = img
     }
     //creacion de cards
@@ -41,10 +41,10 @@ class Juegos{
 
 let juegosdeMesa = new Juegos( '0','Juego de Mesa', 3700, '../imagenes/juegos.jpg')
 let plaza= new Juegos( '1', 'Plaza', 18500, '../imagenes/peloterito-y-auto.jpg')
-let castillo = new Juegos( '2','Castillo', 8000, '../imagenes/castillos.jpg')
+let castillo = new Juegos( '2','Castillo', 8000,'../imagenes/castillos.jpg')
 let tobogan = new Juegos( '3','Tobogan', 23500, '../imagenes/tobogan.jpg')
-let plazaBlanda = new Juegos( '4','Plaza Blanda', 36500, '../imagenes/plazablanda.jpg')
-let tejo = new Juegos( '5','Tejo', 13500, '../imagenes/download.jpg')
+let plazaBlanda = new Juegos( '4','Plaza Blanda', 36500,'../imagenes/plazablanda.jpg')
+let tejo = new Juegos( '5','Tejo', 13500,'../imagenes/download.jpg')
 
 productos.push(juegosdeMesa, plaza, castillo, tobogan, plazaBlanda, tejo)
 
@@ -56,6 +56,7 @@ productos.forEach(e => {
 productos.forEach(e => {
     e.agregarCarrito()
 })
+
 
 botonVaciar.addEventListener('click', () => {
     carrito.length = 0
@@ -75,13 +76,24 @@ function agregarAlCarrito(producto) {
 }
 actualizarCarrito()
 console.log(carrito)
-contador.innerHTML = carrito.reduce((acc, prod) => acc + prod.cantidad,0)
-}    
+}   
+
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+
+    const indice = carrito.indexOf(item) //Busca el elemento q yo le pase y nos devuelve su indice.
+
+    carrito.splice(indice, 1) //Le pasamos el indice de mi elemento ITEM y borramos 
+    // un elemento 
+    actualizarCarrito() //LLAMAMOS A LA FUNCION QUE CREAMOS EN EL TERCER PASO. CADA VEZ Q SE 
+    //MODIFICA EL CARRITO
+    console.log(carrito)
+}
 
 const actualizarCarrito = () => {
     contenedorCarrito.innerHTML = "" 
-
-    //estructura de cada producto en el contenedor carrito
+//estructura de cada producto en el contenedor carrito
     carrito.forEach((prod) => {
         const div = document.createElement('div')
         div.className = ('productoEnCarrito')
@@ -103,13 +115,13 @@ const actualizarCarrito = () => {
     
 }
 
- const alquilar = document.getElementById('alquilar')
+const alquilar = document.getElementById('alquilar')
 
- alquilar.onclick = () => {
-     let totalAlquiler = 0
-     carrito.forEach ((prod)=>{
-         totalAlquiler = totalAlquiler + prod.precio
-   })
+alquilar.onclick = () => {
+    let totalAlquiler = 0
+    carrito.forEach ((prod)=>{
+    totalAlquiler = totalAlquiler + prod.precio
+})
 
     swal.fire({
         title: 'El total del alquiler es de',
